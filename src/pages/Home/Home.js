dae91c5030e69ac50e90c5423da5bf1b79fc392e
Ana Carolina <84184662+aninhaf1@users.noreply.github.com>
2023-10-27 14:34:30 -0300

    frontend

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+
+import Card from "../../componentes/Card/Card";
+import HeaderPerfil from "../../componentes/Header/HeaderPerfil";
+
+import { SectionStyle } from "./style";
+
+
+
+function Home() {
+
+
+  
+
+
+    return (
+        <>
+            <HeaderPerfil titulo="Questions"/>
+
+            <SectionStyle>
+                {/* <Menu /> */}
+                <Card />
+
+                {/* <Historicos /> */}
+
+            </SectionStyle>
+
+        </>
+    )
+}
+
+
+export default Home
